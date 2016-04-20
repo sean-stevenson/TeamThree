cc7d000ea3098c51b57e6b23378e563adf3bc53c
sean-stevenson <msd.sean@clear.net.nz>
2016-04-20 21:49:46 +1200

    Added rough camera code

--- main.c
+++ main.c
@@ -19,3 +19,27 @@ int main (){
 
 
 return 0;}
+
+/**Some rough code to move along the line of tape, developed from the lecture
+ * take_picture();
+ * int sum = 0;
+ * int i;
+ * (i = 0, i < 320, i++){//Less than 320 as the image is 320 pixels across
+ * int col = get_pixel(120,i,3);
+ *    if(w < 230){//checks the color recieved is enough "white" so no glossy surfaces are detected
+         w = 0;
+      }
+      else{
+         w = 1;
+      }
+      sum = sum + (i-160)*w;//Traverses the pixels along row 120, in increments, using -160_0_160 as the boundaries
+     if(sum > 0){
+       turn_left();//Needs to be improved/made a method
+     {
+     else if(sum < 0){
+      turn_right();//Needs to be improved/made a method
+     {
+     else if(sum == 1)
+      set_motor(1,1,255)
+      set_motor(2,1,255)
+     }
