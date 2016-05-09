d90fea2aa8bb95df3f0c62d1d6910c5b259868b4
cbrigden <brigden.callum@gmail.com>
2016-05-09 14:52:08 +1200

    moved movement methods to the top
    Methods must be declared before using them in C

--- main.c
+++ main.c
@@ -4,8 +4,28 @@
 //these load specific methods from the ENGR101 library
 extern "C" int init();
 extern "C" int Sleep( int sec , int usec );
-extern "C" int set_motor( int motor , int speed ); //NOW FIXED 
+extern "C" int set_motor( int motor , int speed ); //NOW FIXED
 extern "C" char get_pixel(int row, int col, int color);
+
+/**ROUGH CODES FOR MOVEMENT*/
+    int turn_right(void)
+    {
+    set_motor(1,100);
+    Sleep(0,500000);
+    }
+    int turn_left(void)
+    {
+    set_motor(2,-100);
+    Sleep(0,500000);
+    }
+    int move_forward(void)
+    {
+    set_motor(1,200);
+    set_motor(2,-200);
+
+    }
+
+
 int main (){
    // This sets up the RPi hardware and ensures
    // everything is working correctly
@@ -14,48 +34,37 @@ int main (){
    // in one direction at MAX speed .
    int take_picture();
 
-int sum = 0;      /**the white line*/
+int sum = 0;      /**the white line*/ //not used in current code.
 int i;
 int kp;
+int white_threshold[1];
+int col [15];
+int w;
 
-for(i = 0; i < 15; i++){  /**Less than 320 as the image is 320 pixels across*/
+    for(i = 0; i < 15; i++){  /**Less than 320 as the image is 320 pixels across*/
 
-col[i] = get_pixel(320/16 * i,120,3);
-if(col[i] < 220){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
-    white_threshold[i] = 1;
-}else{
-    white_threshold[i] = 0;
-}
-for(w = 0; w > 15; w++){
-if(w > 8 && white_threshold[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
-      //kp = w/5;
-      turn_right();
-}else if(w < 7 && white_threshold[w] == 1){    /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
-      //kp = w/9; some constant that changes depending on where the array is at, if it is at the lower bounds it will be a greater value
-      turn_left();          
-}else if((w == 8 || w == 7) && white_threshold[w] = 1){
-      move_forward();
-}
-}
+    col [i] = get_pixel(320/16 * i,120,3);
+        if(col[i] < 220){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
+            white_threshold[i] = 1;
+            }
+        else{
+            white_threshold[i] = 0;
+            }
+    for(w = 0; w > 15; w++){
+        if(w > 8 && white_threshold[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
+                //kp = w/5;
+            turn_right();
+            }
+        else if(w < 7 && white_threshold[w] == 1){    /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
+        //kp = w/9; some constant that changes depending on where the array is at, if it is at the lower bounds it will be a greater value
+            turn_left();
+            }
 
-
-return 0;}
-
-/**ROUGH CODES FOR MOVEMENT*/
-
-int turn_left(void){
-set_motor(2,-100);
-Sleep(0,500000);
-}
-
-int turn_right(void){
-set_motor(1,100);
-Sleep(0,500000);
-}
-
-int move_forward(void){
-set_motor(1,200);
-set_motor(2,-200);
-
+        else if((w == 8 || w == 7) &&((white_threshold)[w] = 1))
+            {
+            move_forward();
+            }
+        }
+return 0;
 }
 
