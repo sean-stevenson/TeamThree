0287a5aefbc5e4fabb06fd39a9b039eca3b83f48
cbrigden <brigden.callum@gmail.com>
2016-05-11 10:25:28 +1200

    small fixes + indentation

--- main.cpp
+++ main.cpp
@@ -24,26 +24,26 @@ int openDoor(){
 }
 
 int turn_left(void){
-set_motor(2,-100);
-Sleep(0,500000);
+    set_motor(2,-100);
+    Sleep(0,500000);
 }
 
 int turn_right(void){
-set_motor(1,100);
-Sleep(0,500000);
+    set_motor(1,100);
+    Sleep(0,500000);
 }
 
 int move_forward(void){
-set_motor(1,200);
-set_motor(2,-200);
+    set_motor(1,200);
+    set_motor(2,-200);
 }
 
 int main (){
    // This sets up the RPi hardware and ensures
    // everything is working correctly
    init();
-int doorOpen = openDoor();
-while(doorOpen != 0){
+    int doorOpen = openDoor();
+    while(doorOpen != 0){
       doorOpen = openDoor();
     }
     //execute movement
@@ -63,27 +63,27 @@ int col[16];
 
 
 for(i = 1; i < 16; i++){  /**Less than 320 as the image is 320 pixels across*/
-
-col[i] = get_pixel(320/16 * i,120,3);
-if(col[i] < 220){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
-    white_threshold[i] = 1;
-}else{
+    col[i] = get_pixel(320/16 * i,120,3);
+    if(col[i] < 220){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
+        white_threshold[i] = 1;
+    }
+    else{
     white_threshold[i] = 0;
-}
-for(w = 0; w > 15; w++){
-
-if(white_threshold[7] == 1){
-    move_forward();
-}
-else if(w < 6 && white_threshold[w] == 1){
-    turn_left();
-}
-else if(w > 9 && white_threshold[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
-      //kp = w/5;
-      turn_right();
-}   /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
+    }
+    for(w = 0; w < 15; w++){
+        if(white_threshold[7] == 1){
+            move_forward();
+        }
+        else if(w < 6 && white_threshold[w] == 1){
+            turn_left();
+        }
+        else if(w > 9 && white_threshold[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
+            //kp = w/5;
+            turn_right();
+        }   /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
       //kp = w/9; some constant that changes depending on where the array is at, if it is at the lower bounds it will be a greater value
-}
+    }
+
 }
 return 0;
 }
