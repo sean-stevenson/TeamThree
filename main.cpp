5c3bbf8adb9aeb64cdb78527dbd46961e471c5ca
tanoielis <eli.tanoi@gmail.com>
2016-05-11 11:02:39 +1200

    Added the new motor code

--- main.cpp
+++ main.cpp
@@ -23,71 +23,70 @@ int openDoor(){
     }
 }
 
-int turn_left(void){
-    set_motor(2,-100);
-    Sleep(0,500000);
+void turn_left(){
+    set_motor(2,-50);
 }
 
-int turn_right(void){
-    set_motor(1,100);
-    Sleep(0,500000);
+void turn_right(){
+    set_motor(1,50);
 }
 
-int move_forward(void){
-    set_motor(1,200);
-    set_motor(2,-200);
+void move_forward(){
+    set_motor(1,50);
+    set_motor(2,-50);
 }
 
 int main (){
    // This sets up the RPi hardware and ensures
    // everything is working correctly
-   init();
-    int doorOpen = openDoor();
-    while(doorOpen != 0){
-      doorOpen = openDoor();
-    }
-    //execute movement
+    init();
+    
+    //int doorOpen = openDoor();
+    //while(doorOpen != 0){
+    //  doorOpen = openDoor();
+    //}
+    
 
-   // Sets the motor connected to pin 1 to rotate
-   // in one direction at MAX speed .
-   int take_picture();
 
-int sum = 0;      /**the white line*/
-int i;
-int w;
-int kp;
-int white_threshold[16];
-int col[16];
+    int whiteThreshold = 220;
+    int pxCount = 16;               // This is the amount of pixels we will use from the photo
+    int pixelColour[pxCount];   // Holds the value of the selected pixel (white[1]/black[0])
+    int col[pxCount];
+    int running = 1;
 
 
 
+    while(running == 1){
+        
+    int take_picture();
+    
+    for(int i = 0; i < pxCount; i++){  /**Less than 320 as the image is 320 pixels across*/
 
-for(i = 1; i < 16; i++){  /**Less than 320 as the image is 320 pixels across*/
-    col[i] = get_pixel(320/16 * i,120,3);
-    if(col[i] < 220){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
-        white_threshold[i] = 1;
-    }
-    else{
-    white_threshold[i] = 0;
-    }
-    for(w = 0; w < 15; w++){
-        if(white_threshold[7] == 1){
-            move_forward();
+            col[i] = get_pixel(320/pxCount * i,120,3);
+            if(col[i] > whiteThreshold){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
+                pixelColour[i] = 1;
+            }else{
+                pixelColour[i] = 0;
+            }
         }
-        else if(w < 6 && white_threshold[w] == 1){
-            turn_left();
+        for(int w = 0; w < pxCount; w++){
+
+            if(pixelColour[(pxCount / 2)-1] == 1 || pixelColour[(pxCount / 2)+1 == 1]){
+                move_forward();
+            }
+            else if(w < (pxCount / 2)-2 && pixelColour[w] == 1){
+                turn_left();
+            }
+            else if(w > (pxCount / 2)+2 && pixelColour[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
+                turn_right();
+            }  
         }
-        else if(w > 9 && white_threshold[w] == 1){  /**Fine tuned points where robot travels, will need to be tested*/
-            //kp = w/5;
-            turn_right();
-        }   /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
+    }
+    return 0;
+}
+
+   /**0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0 <-- pixel array taken to determine if a point is white or not, traverse to find if a section of the picture is white*/
       //kp = w/9; some constant that changes depending on where the array is at, if it is at the lower bounds it will be a greater value
-    }
-
-}
-return 0;
-}
-}
 
 
 
