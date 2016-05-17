ab5ba4f53eea615ac2275dec72127201a509c7cd
cbrigden <brigden.callum@gmail.com>
2016-05-18 10:55:50 +1200

    Propper indentation

--- main.cpp
+++ main.cpp
@@ -9,47 +9,47 @@ extern "C" char get_pixel(int row, int col, int color);
 extern "C" int open_screen_stream();
 extern "C" int update_screen();
 int main (){
-init();
-int sum = 0;      /**the white line*/
-int white_threshold[16];
-int col[16];
-int i;
-int w;
+    init();
+    int sum = 0;      /**the white line*/
+    int white_threshold[16];
+    int col[16];
+    int i;
+    int w;
 
-int kp = 0.5;
-int pSignal = 0;
-open_screen_stream();
-while(1==1){
-take_picture();
-update_screen();
-for(i = 0; i < 16; i++){  /**Less than 320 as the image is 320 pixels across*/
+    int kp = 0.5;
+    int pSignal = 0;
+    open_screen_stream();
+    while(1==1){
+        take_picture();
+        update_screen();
+        for(i = 0; i < 16; i++){  /**Less than 320 as the image is 320 pixels across*/
 
-col[i] = get_pixel(320/16 * i,120,3);
-    if(col[i] > 190){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
-        white_threshold[i] = 1; //assigns 1 where line is located
-    }else{
-        white_threshold[i] = 0; //assigns 0 to where it is more black
-    }
-    sum = sum + (i - 7) * white_threshold[i]; //will find a value that is either positive or negative, which will determine how far away from the line it is and which side of line.
-    pSignal = sum * kp*8; 
-    if(sum < 0){//left
-        set_motor(1, pSignal);//need to work out which is which motor
-         printf("Left %d\n", pSignal);
-    }
-    else if(sum > 0){//right
-    set_motor(2,-1 * pSignal);//need to work out which is which motor
-     printf("Right %d\n", pSignal);
+            col[i] = get_pixel(320/16 * i,120,3);
+            if(col[i] > 190){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
+                white_threshold[i] = 1; //assigns 1 where line is located
+            }else{  
+                white_threshold[i] = 0; //assigns 0 to where it is more black
+            }
+            sum = sum + (i - 7) * white_threshold[i]; //will find a value that is either positive or negative, which will determine how far away from the line it is and which side of line.
+            pSignal = sum * kp*8; 
+            if(sum < 0){//left
+                set_motor(1, pSignal);//need to work out which is which motor
+                 printf("Left %d\n", pSignal);
+            }
+            else if(sum > 0){//right
+                set_motor(2,-1 * pSignal);//need to work out which is which motor
+                printf("Right %d\n", pSignal);
+            
+            }
+            else{
+                set_motor(1, pSignal);
+                set_motor(2,-1 * pSignal);
         
-    }
-    else{
-        set_motor(1, pSignal);
-        set_motor(2,-1 * pSignal);
+                 printf("Forward %d\n", pSignal);
+            }
     
-         printf("Forward %d\n", pSignal);
-    }
-   
-    }
-     printf("Running \n");//testing speed value going to wheels for means of finding way to slow it down.
+        }
+         printf("Running \n");//testing speed value going to wheels for means of finding way to slow it down.
     
     }
     return 0;
