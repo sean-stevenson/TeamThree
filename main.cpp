7a1988c19377368e62155393cf5f40e55d3a45a7
sean-stevenson <msd.sean@clear.net.nz>
2016-05-18 14:08:16 +1200

    revamped code to past iteration, from the beginning

--- main.cpp
+++ main.cpp
@@ -9,64 +9,42 @@ extern "C" char get_pixel(int row, int col, int color);
 extern "C" int open_screen_stream();
 extern "C" int update_screen();
 
-int pSignal = 0;
-
-int turn_right(){
-    set_motor(2,-1 * pSignal);//need to work out which is which motor
-     printf("Right %d\n", pSignal);
-}
-
-int turn_left(){
-     set_motor(1, pSignal);//need to work out which is which motor
-         printf("Left %d\n", pSignal);
-}
-
-
-
 int main (){
 init();
+int pSignal = 0;
 int sum = 0;      /**the white line*/
-int white_threshold[16];
-int col[16];
+int white_threshold = 100;
 int i;
 int w;
+int totalSum = 0;
+int numfound = 0;
 
-int kp = 0.5;
-
-for(int time=0;time<20;time++){ 
-take_picture();
-for(i = 0; i < 16; i++){  /**Less than 320 as the image is 320 pixels across*/
-
-    sum = get_pixel(320/16 * i,120,3);
-    if(sum > 100){     /**checks the color recieved is enough "white" so no glossy surfaces are detected*/
-        white_threshold[i] = 1; //assigns 1 where line is located
-    }
-    else{
-        white_threshold[i] = 0; //assigns 0 to where it is more black
-    }
-    sum = sum + ((i - 7) * white_threshold[i]); //will find a value that is either positive or negative, which will determine how far away from the line it is and which side of line.
-    pSignal = sum * kp*8;
-}
-    
-    for(int w = 0; w < 16; w++){
-    if(sum < 0){//left
-        turn_left();
-    }
-    if(sum > 0){//right
-    turn_right();
-        
-    }
-    if(sum == 0){
-        set_motor(1, pSignal);
-        set_motor(2,-1 * pSignal);
-    
-         printf("Forward %d\n", pSignal);
-    }
-    }
-   
-
-     printf("Running \n");//testing speed value going to wheels for means of finding way to slow it down.
-    Sleep(0, 500000);
+float kP = 0.3;
+while(1==1){
+    take_picture();
+        for(i = 0; i < 320; i++){  /**Less than 320 as the image is 320 pixels across*/
+            sum = get_pixel(120,i,3);
+                if(sum > white_threshold){  
+                    w = 1;
+                }
+                else{
+                    w = 0;
+                }
+            totalSum = totalSum + (i - 160) * w;
+        }
+        signalE = 160 - totalSum;
+        P = kP * signalE;
+        motorS = abs(totalSum);
+            if(pSignal < 0){//left
+                set_motor(1, motorS);//maybe need a constant instead
+                set_motor(2,-1 * motorS + P);
+                Sleep(0, 100000);
+            }
+            else if(pSignal > 0){//right
+                set_motor(1, motorS + P);
+                set_motor(2,-1 * motorS);
+                Sleep(0, 100000);
+            }
 }
     return 0;
 }
