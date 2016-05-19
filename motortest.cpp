a3d9e5b120ce375e75ce47814a2afd4788aae92e
sean-stevenson <msd.sean@clear.net.nz>
2016-05-19 14:18:10 +1200

    test case

---
+++ motortest.cpp
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <time.h>
+/**these load specific methods from the ENGR101 library*/
+extern "C" int init();
+extern "C" int Sleep( int sec , int usec );
+extern "C" int set_motor( int motor , int speed );
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int color);
+extern "C" int open_screen_stream();
+extern "C" int update_screen();
+
+int main (){
+init();
+int z = 0;
+    while(z< 100){
+    
+                set_motor(1, 40);
+                set_motor(2, -40);
+                Sleep(0, 500000);
+                z++;
+                }
+                set_motor(1, 0);
+                set_motor(2, 0);
+                return 0;
+}
