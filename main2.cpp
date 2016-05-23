974e88451554dceede85db06cacd0a06ef4ed3bb
sean-stevenson <msd.sean@clear.net.nz>
2016-05-23 13:21:21 +1200

    0.2

--- main2.cpp
+++ main2.cpp
@@ -17,7 +17,7 @@ int white_threshold = 100;
 /**Value pixels need to be over to be considered "white"*/
 int w = 0;
 int num = 0;
-double kP = 0.28;
+double kP = 0.2;
 int pSignal = 0;
 int z = 0;
 /**P in PID
