f1b32359bf0be1877010aa626a3f91dd625f1daa
sean-stevenson <msd.sean@clear.net.nz>
2016-05-23 13:11:46 +1200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -46,13 +46,14 @@ while(z < 200){
             totalSum = totalSum + ((i - 160) * w);
             
         }
-        if(num < 18){
+        if(num < 25){
                 set_motor(1, -35);/**Minuses values if signal is minus it is double negative therefore positive*/
                 set_motor(2, 35.5);
                 Sleep(0, 500000);
                 continue;
         }
         else if(num != 0){
+            printf("totalSum %d\n", totalSum);
             eValue = totalSum/num;
             printf("num %d\n", num);
             printf("eValue %d\n", eValue);
