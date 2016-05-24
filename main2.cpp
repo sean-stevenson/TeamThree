08d176e644db563ffd46f3456acfb2e9b01d0e49
sean-stevenson <msd.sean@clear.net.nz>
2016-05-24 20:34:41 +1200

    added comments

--- main2.cpp
+++ main2.cpp
@@ -27,15 +27,15 @@ int openDoor(){
 int main (){
 init();
 
-int sum = 0;      
-int white_threshold = 100; 
+int sum = 0;//get pixel value which returns values from 0 to 255 depending on the whiteness      
+int white_threshold = 100;//Threshold of white, i.e. from the 0 to 255 only values above this are detected 
 int w = 0;
 int num = 0;
-float kP = 0.13;
+float kP = 0.13;//Prop constant which scales with error signal
 //float kD = 5;
-int pastError = 0;
-int currentError = 0;
-int eValue = 0;
+int pastError = 0;//Past error to work out kD
+int currentError = 0;//Absolute of error signal - will need to check that works
+int eValue = 0;//Average value of error either side
 /*
 int doorOpen = openDoor();
 while(doorOpen != 0){
@@ -44,44 +44,45 @@ while(doorOpen != 0){
 Sleep(3,0);
 */
 while(1){
-    take_picture();
     int totalSum = 0;
     int pSignal = 0;
     int dSignal = 0;
     int sum = 0;
     int num = 0;
     int eValue = 0;
+    take_picture();//Takes picture and sets all the variables to 0
         for(int i = 0; i < 320; i++){  /**Less than 320 as the image is 320 pixels across*/
-            sum = get_pixel(i, 1, 3);
-                if(sum > white_threshold){  
+            sum = get_pixel(i, 1, 3);//Gets pixel at row 1 as it goes from 1 to 240
+                if(sum > white_threshold){  //If value greater than threshold make it 1 and add to num
                     w = 1;
                     num++; //num increases when a white pixel is found
+                    
                 }
                 else{
                     w = 0;
                 }
-            totalSum = totalSum + ((i - 160) * w);
+                totalSum = totalSum + ((i - 160) * w);//Takes the position of the i and adds to a total
         }
-        if(num < 20){ //Low amount of white pixels found turn left?
+        if(num < 20){ //If not enough pixels are found, reverse and reset
                 set_motor(1, -40);
                 set_motor(2, 40.5);
                 Sleep(0, 100000);
                 continue;
         }
         else if(num != 0){
-            eValue = totalSum/num;
-            pSignal = eValue*kP;
+            eValue = totalSum/num;//Finds average of a point sat -130 or 50
+            pSignal = eValue*kP;//Times it by kP to get a value scaled with the e sginal
             //currentError = abs(eValue);
             //dSignal = abs((currentError - pastError)*kD);
             //pastError = eValue;
             if(pSignal > 0){/**right*/
                 printf("right %d\n", pSignal);
-                set_motor(1, (35 + pSignal));/**Minuses values if signal is minus it is double negative therefore positive*/
+                set_motor(1, (35 + pSignal));
                 // + dSignal
                 set_motor(2, -35.5);
                 Sleep(0, 5000);
             }
-            else if(pSignal < 0){/**Prioritises left turns first*/
+            else if(pSignal < 0){/**left*/
                 printf("left %d\n", pSignal);
                 set_motor(1, 35);/**From a few calculations 40 seems roughly right, max value is 70ish*/
                 set_motor(2, -(35.5 - pSignal));/**Minuses values if signal is minus it is double negative therefore positive*/
