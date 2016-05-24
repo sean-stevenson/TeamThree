7066e551c266be56bb392c8d8c57ac333748f5e1
sean-stevenson <msd.sean@clear.net.nz>
2016-05-25 11:58:37 +1200

    moved all values into move()

--- main2.cpp
+++ main2.cpp
@@ -26,8 +26,13 @@ int openDoor(){
 
 int main (){
 init();
+int doorOpen = openDoor();
+move();
+    return 0;
+}
 
-int sum = 0;//get pixel value which returns values from 0 to 255 depending on the whiteness      
+int move(){
+    int sum = 0;//get pixel value which returns values from 0 to 255 depending on the whiteness      
 int white_threshold = 130;//Threshold of white, i.e. from the 0 to 255 only values above this are detected 
 int w = 0;
 int num = 0;
@@ -37,12 +42,6 @@ float kD = 0.001;
 int pastError = 0;//Past error to work out kD
 int currentError = 0;//Absolute of error signal - will need to check that works
 int eValue = 0;//Average value of error either side
-
-//int doorOpen = openDoor();
-move();
-    return 0;
-}
-int move(){
 Sleep(2,0);
 while(1){
     int totalSum = 0;
