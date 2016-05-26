f761faea7c94c3ae169943310522635797ffee70
tanoielis <eli.tanoi@gmail.com>
2016-05-26 13:43:32 +1200

    Basic IR detecting wall code
    This can be implemented with quad 3 code to complete quad 4. It will tell us where the walls are: it can be directly added to the main code or called as a function. Make sure to add the two functions added in this code.

---
+++ quad4draft.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+extern "C" int init(int d_lev);
+extern "C" int read_analog(int ch_adc);
+extern "C" int select_IO(int chan, int direct);
+
+int maze(){
+
+  select_IO(0, 1);  //Assuming the left IR is in analogue 0
+  select_IO(1, 1);  //And right is in 1
+  
+  int leftSensor = read_analog(0);  //The IR return a percentage (0 to 1)
+  int rightSensor = read_analog(1); //Of the voltage passed through
+  int expectedWallDistance = 0.3;    //This value is random at the moment and needs to be tested and changed
+  
+  if(leftSensor < expectedWallDistance){
+    printf("Wall to left\n");
+  }else{
+    printf("No wall to left\n");
+  }
+  if(rightSensor < expectedWallDistance){
+    printf("Wall to right\n");
+  }else{
+    printf("No wall to right\n");
+  }
+  return 0;
+  
+}
+
+int main(){
+  maze();
+}
