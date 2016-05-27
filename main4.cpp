b868f7efc40303b635f0f0fd480ee5ee44125d0a
cbrigden <brigden.callum@gmail.com>
2016-05-27 14:17:23 +1200

    k changes

--- main4.cpp
+++ main4.cpp
@@ -24,7 +24,7 @@ int openDoor(){
 
 int move(){
     int white_threshold = 130;//Threshold of white, i.e. from the 0 to 255 only values above this are detected
-    float kP = 0.3;//Prop constant which scales with error signal
+    float kP = 0.2;//Prop constant which scales with error signal
     float kD = 0.005;
     int pastError = 0;//Past error to work out kD
     int currentError = 0;//Absolute of error signal - will need to check that works
